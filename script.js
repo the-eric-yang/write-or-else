12960f9fb7e9f0fc3c7e1410c73702a64f800107
Eric Yang <149550054+the-eric-yang@users.noreply.github.com>
2024-08-08 21:54:36 -0400

    Update script.js
    forgot to add wordGoal as a local variable. passed it in as a parameter

--- script.js
+++ script.js
@@ -8,19 +8,19 @@ function writeOrElse() {
     let numGracePeriods = (queries.get('target') * 60) / grace;
     let wordInterval = wordGoal / numGracePeriods;
     let textbox = document.getElementById("writing-box");
-    setTimeout(1000 * grace, tracker(grace, wordInterval, 0, numGracePeriods));
+    setTimeout(1000 * grace, tracker(grace, wordInterval, 0, numGracePeriods, wordGoal));
   }
 }
 
-function tracker(gracePeriod, wordInterval, numPeriods, maxPeriods) {
+function tracker(gracePeriod, wordInterval, numPeriods, maxPeriods, wordGoal) {
   let textbox = document.getElementById("writing-box");
   if(numPeriods * wordInterval > countWords(textbox.value)) {
         alert("Write Faster!!!!");
         // more "punishments" can and will be added later on
         if (numPeriods < maxPeriods) {
-          setTimeout(gracePeriod * 1000, tracker(gracePeriod, wordInterval, numPeriods + 1, maxPeriods));
+          setTimeout(gracePeriod * 1000, tracker(gracePeriod, wordInterval, numPeriods + 1, maxPeriods, wordGoal));
         } else {
-          setTimeout(gracePeriod * 1000, tracker(gracePeriod, wordInterval, maxPeriods, maxPeriods));
+          setTimeout(gracePeriod * 1000, tracker(gracePeriod, wordInterval, maxPeriods, maxPeriods, wordGoal));
         }
       }
   else if(countWords(textbox.value) > wordGoal)  {
